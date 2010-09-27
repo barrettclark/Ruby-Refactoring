0d991ae447514e111b74c140f30465282f62eae3
Barrett Clark <barrett@websitesbybarrett.com>
2010-09-26 22:15:54 -0500

    Finished out Rental test (with bonus bugfix)

--- ch01/rental.rb
+++ ch01/rental.rb
@@ -17,5 +17,6 @@ class Rental
       result += 1.5
       result += (days_rented - 3) * 1.5 if days_rented > 3
     end
+    result
   end
 end
--- ch01/test/test_rental.rb
+++ ch01/test/test_rental.rb
@@ -3,13 +3,26 @@ require 'test_helper'
 class RentalTest < Test::Unit::TestCase
   def setup
     @movie = Movie.new('Gone With The Wind', Movie::REGULAR)
+    @rental = Rental.new(@movie, 3)
   end
   def test_new_rental
-    rental = Rental.new(@movie, 3)
-    assert_equal 'Gone With The Wind', rental.movie.title
-    assert_equal 3, rental.days_rented
+    assert_equal 'Gone With The Wind', @rental.movie.title
+    assert_equal 3, @rental.days_rented
   end
   def test_charge
-    # TODO: write this
+    assert_equal 3.5, @rental.charge
+
+    rental = Rental.new(@movie, 1)
+    assert_equal 2, rental.charge
+    
+    new_release_movie = Movie.new('New Hotness', Movie::NEW_RELEASE)
+    rental = Rental.new(new_release_movie, 2)
+    assert_equal 6, rental.charge
+    
+    kid_movie = Movie.new('Bambi', Movie::CHILDRENS)
+    rental = Rental.new(kid_movie, 3)
+    assert_equal 1.5, rental.charge
+    rental = Rental.new(kid_movie, 5)
+    assert_equal 4.5, rental.charge
   end
 end
