3029aee2dd94b830583f1b7a289000262ea847b9
Barrett Clark <barrett@websitesbybarrett.com>
2010-09-27 22:19:29 -0500

    Created Movie pricing classes

--- ch01/movie.rb
+++ ch01/movie.rb
@@ -4,28 +4,51 @@ class Movie
   CHILDRENS = 2
   
   attr_reader :title
-  attr_accessor :price_code
+  attr_reader :price_code
+  
+  def price_code=(value)
+    @price_code = value
+    @price = case price_code
+      when REGULAR: RegularPrice.new
+      when NEW_RELEASE: NewReleasePrice.new
+      when CHILDRENS: ChildrensPrice.new
+    end
+  end
   
   def initialize(title, price_code)
-    @title, @price_code = title, price_code
+    @title, self.price_code = title, price_code
   end
 
   def charge(days_rented)
-    result = 0
-    case price_code
-    when REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when NEW_RELEASE
-      result += days_rented * 3
-    when CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    @price.charge(days_rented)
   end
 
   def frequent_renter_points(days_rented)
     (price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
+  end
+end
+
+# 
+# New classes to model pricing (using the State Pattern)
+# 
+class RegularPrice
+  def charge(days_rented)
+    result = 2
+    result += (days_rented - 2) * 1.5 if days_rented > 2
+    result
+  end
+end
+
+class NewReleasePrice
+  def charge(days_rented)
+    days_rented * 3
+  end
+end
+
+class ChildrensPrice
+  def charge(days_rented)
+    result = 1.5
+    result += (days_rented - 3) * 1.5 if days_rented > 3
+    result
   end
 end
